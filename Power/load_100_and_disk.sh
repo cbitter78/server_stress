66b3074c74618f9549dbffe32d66a0954d15fa21
Charles Bitter <charles_bitter@cable.comcast.com>
2023-06-21 14:38:59 -0400

    setting things up

--- Power/load_100_and_disk.sh
+++ Power/load_100_and_disk.sh
@@ -1,73 +1,8 @@
 #! /bin/sh
 
-stress-ng --cpu 0 --cpu-load 99 --cpu-method loop --timeout 30 --metrics --vm-bytes 1G &
+TIMEOUT=30
 
-
-fio \
---filename=/dev/nvme2n1 \
---filename=/dev/nvme3n1 \
---filename=/dev/nvme4n1 \
---filename=/dev/nvme5n1 \
---filename=/dev/nvme6n1 \
---filename=/dev/nvme7n1 \
---filename=/dev/nvme8n1 \
---direct=1 \
---rw=randread \
---bs=4k \
---ioengine=libaio \
---iodepth=256 \
---runtime=60 \
---numjobs=4 \
---eta-newline=1 \
---name=iops-test-job &
-
-fio \
---filename=/dev/nvme2n1 \
---filename=/dev/nvme3n1 \
---filename=/dev/nvme4n1 \
---filename=/dev/nvme5n1 \
---filename=/dev/nvme6n1 \
---filename=/dev/nvme7n1 \
---filename=/dev/nvme8n1 \
---direct=1 \
---rw=randread \
---bs=4k \
---ioengine=libaio \
---iodepth=256 \
---runtime=60 \
---numjobs=4 \
---eta-newline=1 &
-
-fio \
---filename=/dev/nvme2n1 \
---filename=/dev/nvme3n1 \
---filename=/dev/nvme4n1 \
---filename=/dev/nvme5n1 \
---filename=/dev/nvme6n1 \
---filename=/dev/nvme7n1 \
---filename=/dev/nvme8n1 \
---direct=1 \
---rw=randread \
---bs=4k \
---ioengine=libaio \
---iodepth=256 \
---runtime=60 \
---numjobs=4 \
---eta-newline=1 &
-
-fio \
---filename=/dev/nvme2n1 \
---filename=/dev/nvme3n1 \
---filename=/dev/nvme4n1 \
---filename=/dev/nvme5n1 \
---filename=/dev/nvme6n1 \
---filename=/dev/nvme7n1 \
---filename=/dev/nvme8n1 \
---direct=1 \
---rw=randread \
---bs=4k \
---ioengine=libaio \
---iodepth=256 \
---runtime=60 \
---numjobs=4 \
---eta-newline=1 &
+stress-ng --cpu 0 --cpu-load 99 \ 
+          --cpu-method loop --timeout $TIMEOUT \
+          --metrics --vm-bytes 1G &
+fio 4k_8disk.fio --timeout=$TIMEOUT
--- Power/setup.sh
+++ Power/setup.sh
@@ -1,2 +1,2 @@
 apt-get update
-apt-get -y install curl git stress-ng sysstat fio
+apt-get -y install curl git stress-ng sysstat fio vim
