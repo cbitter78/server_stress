527188a8681d0254201e138ebeb31e9ab9d397f5
Charles Bitter <charles_bitter@cable.comcast.com>
2023-06-21 14:40:42 -0400

    setting things up

--- Power/load_100_and_disk.sh
+++ Power/load_100_and_disk.sh
@@ -2,7 +2,5 @@
 
 TIMEOUT=30
 
-stress-ng --cpu 0 --cpu-load 99 \ 
-          --cpu-method loop --timeout $TIMEOUT \
-          --metrics --vm-bytes 1G &
-fio 4k_8disk.fio --timeout=$TIMEOUT
+stress-ng --cpu 0 --cpu-load 99 --cpu-method loop --timeout $TIMEOUT --metrics --vm-bytes 1G &
+fio 4k_8disk.fio --timeout=$TIMEOUT &
