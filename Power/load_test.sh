42bddad3eedb4243d8bc3ce8bc5c7a11984264f4
Charles Bitter <charles_bitter@cable.comcast.com>
2023-06-21 17:26:53 -0400

    setting things up

--- Power/load_test.sh
+++ Power/load_test.sh
@@ -6,7 +6,12 @@ CPU_LOAD="${1:-$DEFAULT_CPU_LOAD_PERCENT}"
 DEFAULT_TIMEOUT_SECONDS=300
 TIMEOUT="${2:-$DEFAULT_TIMEOUT}"
 
-echo "Load testing at $CPU_LOAD% for $TIMEOUT seconds"
+echo "******************************"
+echo "***** Load testing at $CPU_LOAD% for $TIMEOUT seconds"
+echo "******************************"
+echo ""
 
 stress-ng --cpu 0 --cpu-load $CPU_LOAD --cpu-method loop --timeout $TIMEOUT --metrics --vm-bytes 1G &
+stress-ng --vm 8 --vm-bytes $CPU_LOAD --vm-method all --verify -t $TIMEOUT -v
+
 fio 4k_8disk.fio --timeout=$TIMEOUT &
--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 A repo with handy tools to stress a server.
 
 
+
+## Helpful links
+
+- [How to check CPU temperature on Ubuntu Linux](https://www.cyberciti.biz/faq/how-to-check-cpu-temperature-on-ubuntu-linux/#:~:text=Procedure%20for%20monitoring%20CPU%20and,Open%20the%20terminal%20application.&text=Run%20hddtemp%20command%20to%20see,CPU%20temperature%20in%20Ubuntu%20Linux.)
